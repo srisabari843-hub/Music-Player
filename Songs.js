b6a716fbc9b83bc2918336a0fa72a3f05bb03d50
srisabari843-hub <srisabari843@gmail.com>
2026-07-08 10:49:36 +0530

    commit

---
+++ Songs.js
@@ -0,0 +1,53 @@
+
+
+
+const SONGS=[
+    {
+        id:1,
+        title:"Late Static",
+        artist:"Coral Drift",
+        album:"Room Tone",
+        duration:0,
+        color:"#C9A227",
+        src:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    },
+    {
+    id:2,
+    title:"Nine Floors Up",
+    artist : "Nettle & Bone",
+    album : "Walk-Up Buildings",
+    duration:0,
+    color: "#8C6A4F",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+    },
+    {
+        id : 3,
+        title : "Slow Freight",
+        artist: "Coral Drift",
+        album : "Room Tone",
+        duration:0,
+        color:"#5E7A6B",
+        src:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+    },
+    {
+        id:4,
+        title:"Amber Light, No Hurry",
+        artist:"Fenwick Low",
+        album:"Corner Store",
+        duration:0,
+        color:"#B5533C",
+        src:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
+    },
+    {
+     id :5,
+     title : "Back Porch Signal",
+     artist : "Nettle & Bone",
+     album: "Walk-Up Buildings",
+     duration:0,
+     color:"#7A6BAE",
+     src:"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3",
+    },
+                              
+];
+
+
